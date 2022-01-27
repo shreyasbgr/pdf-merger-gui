4fbe68e248b94c140e45e8871a620ed663788204
Shreyas Banagar <shreyasbanagar7@gmail.com>
2022-01-28 00:23:08 +0530

    Modifications to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def list_files_directories(path):
+def get_files_directories(path):
     files=os.listdir(path)
     files_full_path=[os.path.join(path,f) for f in files]
     return files_full_path
@@ -20,9 +20,23 @@ def pdf_merger(paths,output):
         pdf_writer.write(out)
 
 def final_fn():
-    path = r"C:\Users\shrey\Documents\Zoom"
-    paths = list_files_directories(path)
-    pdf_merger(paths, output=os.path.join(path,"merged.pdf"))
+    path=input("Enter the path where at which all the pdf's need to be merged: ")
+    paths = get_files_directories(path)
+    pdf_paths = [p for p in paths if p.endswith('.pdf')]
+    files = [os.path.basename(p) for p in paths]
+    pdf_files = [os.path.basename(p) for p in pdf_paths]
+
+    print("All the files and folders in the current directory are: \n")
+    for f in files:
+        print(f)
+    print()
+
+    print("All the pdf files in the current directory are: \n")
+    for f in pdf_files:
+        print(f)
+    print()
+
+    pdf_merger(pdf_paths, output=os.path.join(path,"merged.pdf"))
 
 final_fn()
 
